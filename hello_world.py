63869cd6a26c999c75bcba4b9329b18ead0f1504
Rukumo <60543417+Rukumo@users.noreply.github.com>
2025-06-24 23:51:18 +0900

    Initial commit: Add Python Hello World program

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+    print("Hello, World!")
+    print("こんにちは、世界！")
+    print("Python Hello World プログラムです")
+
+if __name__ == "__main__":
+    main()
